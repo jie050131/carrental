c430d357155311025b83460786e4b937e978d0ab
Jason <1037724024@qq.com>
2022-05-14 20:59:08 +0800

    test

--- cardemo/src/main/java/com/sj/cardemo/mapper/UserMapper.java
+++ cardemo/src/main/java/com/sj/cardemo/mapper/UserMapper.java
@@ -1,33 +1,35 @@
 package com.sj.cardemo.mapper;
 
-import com.baomidou.mybatisplus.mapper.BaseMapper;
+
 import com.sj.cardemo.bean.User;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserMapper {
+
+public interface UserMapper extends JpaRepository<User, Integer> {
+
+    @Query(value = "select * from user where username=?1 ", nativeQuery = true)
+    public User findUserByName(String username);
+
+    //根据用户名查盐
+    @Query(value = "select salt from user where username=?1", nativeQuery = true)
+    public String saltByUsername(String username);
+
+    //检查用户名是否重复
+    public User findAllByUsername(String username);
+
+    //通过id查name
+    public User findUserById(Integer id);
+
+
+    //查询全部用户
+    @Override
+    @Query(value = "select * from user where perm=0 ", nativeQuery = true)
+    public Page<User> findAll(Pageable pageAble);
 
 }
-//public interface UserMapper extends JpaRepository<User, Integer> {
-//
-//////    @Query(value = "select * from user where username=?1 ", nativeQuery = true)
-//////    public User findUserByName(String username);
-////
-////    //根据用户名查盐
-////    @Query(value = "select salt from user where username=?1", nativeQuery = true)
-////    public String saltByUsername(String username);
-//
-//    //检查用户名是否重复
-//    public User findAllByUsername(String username);
-//
-//    //通过id查name
-//    public User findUserById(Integer id);
-//
-//
-//    //查询全部用户
-//    @Override
-//    @Query(value = "select * from user where perm=0 ", nativeQuery = true)
-//    public Page<User> findAll(Pageable pageAble);
-//
-//}
